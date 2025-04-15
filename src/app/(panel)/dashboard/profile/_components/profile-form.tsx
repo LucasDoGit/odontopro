33c84211d8b3b93174d1178c6047533e9df3dd0e
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-04-15 07:36:41 -0300

    feat(profile): atualizando informações da clínica

--- src/app/(panel)/dashboard/profile/_components/profile-form.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile-form.tsx
@@ -2,6 +2,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
+interface UserProfileFormProps {
+    name: string | null;
+    address: string | null;
+    phone: string | null;
+    status : boolean;
+    timeZone: string | null;
+}
+
 const profileSchema = z.object({
     name: z.string().min(1, { message: "O nome é obrigatório" }),
     address: z.string().optional(),
@@ -10,17 +18,17 @@ const profileSchema = z.object({
     timeZone: z.string().min(1, { message: "O time zone é obrigatório"}),
 })
 
-type ProfileFormData = z.infer<typeof profileSchema>;
+export type ProfileFormData = z.infer<typeof profileSchema>;
 
-export function useProfileForm(){
+export function useProfileForm({ name, address, phone, status, timeZone}: UserProfileFormProps){
     return useForm<ProfileFormData>({
         resolver: zodResolver(profileSchema),
         defaultValues: {
-            name: "",
-            address: "",
-            phone: "",
-            status: "Ativo",
-            timeZone: "",
+            name: name || "",
+            address: address || "",
+            phone: phone || "",
+            status: status ? "active" : "inacative",
+            timeZone: timeZone || "",
         }
     })
 }
