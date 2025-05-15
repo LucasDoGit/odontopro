45ff67dc4cc410ce19bfdb5fe336a97fbd9d017f
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-05-15 01:02:50 -0300

    feat: verificação de permissões e assinatura trial

---
+++ src/utils/permissions/can-create-service.ts
@@ -0,0 +1,48 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { Subscription } from "@prisma/client";
+import { Session } from "next-auth";
+import { getPlan } from "./get-plans";
+import { PLANS } from "../plans";
+import { checkSubscriptionExpired } from "./check-subscription-expired";
+import { ResultPermissionProps } from "./can-permission";
+
+export async function canCreateService(subscription: Subscription | null, session: Session): Promise<ResultPermissionProps>{
+
+    try {
+        const serviceCount = await prisma.service.count({
+            where: {
+                userId: session.user.id
+            }
+        })
+
+        if(subscription && subscription.status === "active"){
+            const plan = subscription.plan
+
+            const planLimits = await getPlan(plan);
+
+            console.log("limites do seu plano", planLimits)
+
+            return {
+                hasPermission: planLimits.maxServices === null || serviceCount <= planLimits.maxServices,
+                planId: subscription.plan,
+                expired: false,
+                plan: PLANS[subscription.plan],
+            }
+        }
+
+        const checkTestLimit = await checkSubscriptionExpired(session);
+
+        return checkTestLimit
+
+    } catch (error) {
+        return {
+            hasPermission: false,
+            planId: "EXPIRED",
+            expired: false,
+            plan: null,
+        }
+    }
+
+}
